735bf295540d72a47dede3ca3beef1091de06b84
adryserage <17680194+adryserage@users.noreply.github.com>
2025-05-12 15:17:59 -0400

    ajouter une classe Java affichant les informations sur l'auteur et nettoyer le format README

---
+++ prenom.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class prenom {
+    public static void main(String[] args) {
+        String message = "Name: Adryan Serage\nGitHub: https://github.com/adryserage";
+        JOptionPane.showMessageDialog(null, message, "About Me", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
